6c1eb14a262308dfe5b36b69e4ec43d773dc80f2
Elvis Santana <elvissantanaguimaraes@gmail.com>
2024-07-01 18:25:22 -0300

    docs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatListModule } from "@angular/material/list"
+import { MatButtonModule, MatButton } from "@angular/material/button"
+import { MatIcon } from "@angular/material/icon"
+import { MatToolbarModule } from "@angular/material/toolbar"
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatListModule,
+    MatButtonModule,
+    MatIcon,
+    MatToolbarModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
