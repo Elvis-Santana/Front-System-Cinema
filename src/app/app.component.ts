8078106c44b21d8f0a2b057f2c85c79425cd4354
Elvis Santana <elvissantanaguimaraes@gmail.com>
2024-07-05 10:53:58 -0300

    feat : add nav-list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatListModule } from "@angular/material/list"
 import { MatButtonModule, MatButton } from "@angular/material/button"
+
 import { MatIcon } from "@angular/material/icon"
 import { MatToolbarModule } from "@angular/material/toolbar"
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './pages/home/home.component';
 
 @Component({
   selector: 'app-root',
@@ -15,6 +17,7 @@ import { HttpClientModule } from '@angular/common/http';
     MatButtonModule,
     MatIcon,
     MatToolbarModule,
+    HomeComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
