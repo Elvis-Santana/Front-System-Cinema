17c0a8615e55709b205b50f7973998e53d6c8186
Elvis Santana <elvissantanaguimaraes@gmail.com>
2024-07-09 11:30:05 -0300

    feat  : add  getFilmes

--- src/app/shared/services/listService/list.service.ts
+++ src/app/shared/services/listService/list.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Observable, map } from 'rxjs';
 import { IProdutora } from '../../interfaces/Produtora.interface';
+import { IFilme } from '../../interfaces/Filme.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +15,14 @@ export class ListService {
   public async GetProdutora(): Promise<Observable<IProdutora[]>> {
     return this.http.get<IProdutora[]>("http://localhost:3000/Produtora").pipe(
       map((res: IProdutora[]) => {
+        console.log(res)
+        return res;
+      })
+    )
+  }
+  public async GetFilme(): Promise<Observable<IFilme[]>> {
+    return this.http.get<IFilme[]>("http://localhost:3000/Filme").pipe(
+      map((res: IFilme[]) => {
         console.log(res)
         return res;
       })
