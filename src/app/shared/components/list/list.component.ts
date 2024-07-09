b6250cb3a7464093868f321d0e00e7d707cd1495
Elvis Santana <elvissantanaguimaraes@gmail.com>
2024-07-09 11:29:16 -0300

    refactor : reformulação card

--- src/app/shared/components/list/list.component.ts
+++ src/app/shared/components/list/list.component.ts
@@ -4,8 +4,12 @@ import { Observable } from 'rxjs';
 import { IProdutora } from '../../interfaces/Produtora.interface';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
-import { CardProdutoraComponent } from '../cardProdutora/cardProdutora.component';
+import { CardProdutoraComponent } from '../card/card-Produtora/cardProdutora.component';
 import { MatCardModule } from '@angular/material/card';
+import { IFilme } from '../../interfaces/Filme.interface';
+import { CardFilmeComponent } from '../card/card-filme/card-filme.component';
+import { Router, RouterModule } from '@angular/router';
+import { PalavrasReservadas } from '../../../../../environment/enum/PalavrasReservadas';
 
 @Component({
   selector: 'app-list-produtora',
@@ -14,19 +18,31 @@ import { MatCardModule } from '@angular/material/card';
     HttpClientModule,
     CommonModule,
     CardProdutoraComponent,
-    MatCardModule
+    CardFilmeComponent,
+    MatCardModule,
+    RouterModule,
   ],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
 export class ListComponent implements OnInit {
 
-  public list$: Observable<IProdutora[]> = new Observable<IProdutora[]>()
+  public listP$: Observable<IProdutora[]> = new Observable<IProdutora[]>()
+  public listF$: Observable<IFilme[]> = new Observable<IFilme[]>()
 
   protected listService = inject(ListService);
+  protected router = inject(Router);
+  protected PalavrasReservadas = PalavrasReservadas;
+  public url: string = "";
 
   async ngOnInit() {
-    this.list$ = (await this.listService.GetProdutora())
+    this.listP$ = (await this.listService.GetProdutora());
+    this.listF$ = (await this.listService.GetFilme());
+
+    this.url = (this.router.url.slice(1,));
+    if (!this.url) {
+
+    }
   }
 
 
