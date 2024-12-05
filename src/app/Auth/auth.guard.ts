9477efbbaa0bde76c457a75412989708bf205790
Elvis Santana <elvissantanaguimaraes@gmail.com>
2024-12-05 08:37:09 -0300

    feat : criar auth

--- src/app/Auth/auth.guard.ts
+++ src/app/Auth/auth.guard.ts
@@ -1,20 +1,27 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
 import { token } from '../shared/Model/tokenModel';
 import { inject } from '@angular/core';
 import { SessaoServiceService } from '../shared/services/sessaoService/sessao-service.service';
 import { ITokenReturn } from '../shared/interfaces/ITokenReturn';
 import { RouterService } from '../shared/services/routerService/router.service';
+
 export const authGuard: CanActivateFn = async (route, state) => {
 
   const token: token = { token: String(localStorage.getItem("token")) }
   const sessaoServiceService = inject(SessaoServiceService);
-  const routerService = inject(RouterService);
-
-  var result: ITokenReturn = await sessaoServiceService.validToken(token);
+  const routerService = inject(RouterService)
 
 
-  if(!result.success)
-    routerService.nav("/login");
+  try {
+    var result = await sessaoServiceService.validToken(token);
 
-  return result.success;
+    return result.success
+  } catch (ex) {
+    routerService.nav('/login')
+    return (ex as ITokenReturn).success
+
+  }
+
+
+
 };
