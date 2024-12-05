a9fc411ec38db73700d1f00e0c3079e700b8aca4
Elvis Santana <elvissantanaguimaraes@gmail.com>
2024-12-05 08:38:00 -0300

    feat : service list.Service

--- src/app/shared/services/listService/list.service.ts
+++ src/app/shared/services/listService/list.service.ts
@@ -1,41 +1,48 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Observable, map } from 'rxjs';
 import { IProdutora } from '../../interfaces/Produtora.interface';
 import { IFilme } from '../../interfaces/Filme.interface';
 import { ICinema } from '../../interfaces/ICinema.interface';
-import { Ports } from '../../../../../environment/enum/Ports';
+import {  _PORTFILMES,  _PORTCINEMA,_PORTPRODUTORA } from '../../../../environments/enum/Ports';
 import { RouterService } from '../routerService/router.service';
+
+
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ListService {
+  private PORT_FILMES = _PORTFILMES;
+  private PORT_CINEMA = _PORTCINEMA;
+  private PORT_PRODUTORA = _PORTPRODUTORA;
+
 
   protected http = inject(RouterService).getHttpClient();
   constructor() { }
 
   public async GetProdutora(): Promise<Observable<IProdutora[]>> {
-    return this.http.get<IProdutora[]>("http://localhost:3000/Produtora").pipe(
+    return this.http.get<IProdutora[]>(String(this.PORT_PRODUTORA.development.JOSN)).pipe(
       map((res: IProdutora[]) => {
-        console.log(res)
         return res;
       })
     )
   }
   public async GetFilme(): Promise<Observable<IFilme[]>> {
-    return this.http.get<IFilme[]>(Ports.Api_CSharp_PortsFilmes).pipe(
+
+    return this.http.get<IFilme[]>(String(this.PORT_FILMES.development.JOSN)).pipe(
       map((res: IFilme[]) => {
-        console.log(res)
         return res;
+
       })
     )
   }
 
   public async CinemaFilme(): Promise<Observable<ICinema[]>> {
-    return this.http.get<ICinema[]>("http://localhost:3000/Cinema").pipe(
+    return this.http.get<ICinema[]>(String(this.PORT_CINEMA.development.JOSN)).pipe(
       map((res: ICinema[]) => {
-        console.log(res)
+            //console.log(res);
         return res;
       })
     )
