b502b2f9a4352919150456045be1311c6669e32d
Elvis Santana <elvissantanaguimaraes@gmail.com>
2024-12-05 08:38:11 -0300

    feat : service router.Service

--- src/app/shared/services/routerService/router.service.ts
+++ src/app/shared/services/routerService/router.service.ts
@@ -1,18 +1,25 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RouterService {
   protected http = inject(HttpClient)
+  protected router = inject(Router)
 
-  constructor() { }
+  constructor() {
+
+
+   }
 
 
   public getHttpClient=()=> this.http;
 
- 
+  public nav=(url:String)=> this.router.navigate([url]);
+
+
+
 
 }
